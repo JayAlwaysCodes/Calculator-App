5a0c2badb81aff95a268e5098e983f77b58c0932
JayAlwaysCodes <johnsonjardocs@gmail.com>
2025-02-06 12:44:00 +0100

    added simple functionality

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ class Main(App):
         self.last_button = None
 
         main_layout = BoxLayout(orientation='vertical')
-        self.solution = TextInput(background_color = 'black', foreground_color = 'white')
+        self.solution = TextInput(background_color = 'black', foreground_color = 'white', font_size = 32, readonly = True, multiline = False, halign = 'right')
 
         main_layout.add_widget(self.solution)
         buttons = [
@@ -36,6 +36,30 @@ class Main(App):
         main_layout.add_widget(equals_button)
 
         return main_layout
+
+    def on_button_press(self, instance):
+        current = self.solution.text
+        button_text = instance.text
+
+        if button_text == 'C':
+            self.solution.text = ''
+        else:
+            if current and (self.last_was_operator and button_text in self.operators):
+                return
+            elif current == '' and button_text in self.operators:
+                return
+            else:
+                new_text = current + button_text
+                self.solution.text = new_text
+        
+        self.last_button = button_text
+        self.last_was_operator = self.last_button in self.operators
+    
+    def on_solution(self, instance):
+        text = self.solution.text
+        if text:
+            solution = str(eval(self.solution.text))
+            self.solution.text = solution
 
 
 if __name__ == '__main__':
